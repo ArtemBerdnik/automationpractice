c392c1dfdfc271d4b07ffa04a8c53bc962ee1470
Artem_Berdnik <Artem_Berdnik@epam.com>
2020-06-22 14:28:58 +0300

    paying around

--- src/main/java/base/TestNGBase.java
+++ src/main/java/base/TestNGBase.java
@@ -3,9 +3,8 @@ package base;
 import com.epam.reportportal.testng.ReportPortalTestNGListener;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Listeners;
+import org.testng.annotations.*;
+import utils.BrowserDriverFactory;
 import utils.TestUtils;
 
 @Listeners({ReportPortalTestNGListener.class})
@@ -15,9 +14,21 @@ public class TestNGBase {
     public static WebDriverWait wait;
 
     @BeforeSuite(alwaysRun = true)
-    public void beforeSuite() {
+    @Parameters({ "browser", "environment" })
+    public void beforeSuite(@Optional("chrome") String browser, @Optional("local") String environment) {
+        // Create Driver
+        BrowserDriverFactory factory = new BrowserDriverFactory(browser);
+        if (environment.equals("grid")) {
+            driver = factory.createDriverGrid();
+        } else {
+            driver = factory.createDriver();
+        }
+
+        // maximize browser window
+        driver.manage().window().maximize();
+
         System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
-        driver = TestUtils.getDriver();
+//        driver = TestUtils.getDriver();
         wait = new WebDriverWait(driver, 8);
     }
 
---
+++ src/main/java/utils/BrowserDriverFactory.java
@@ -0,0 +1,59 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BrowserDriverFactory {
+
+    private ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+    private String browser;
+
+
+    public BrowserDriverFactory(String browser) {
+        this.browser = browser.toLowerCase();
+    }
+
+
+    public WebDriver createDriver() {
+        System.out.println("Starting " + browser + " locally");
+
+        // Creating driver
+        switch (browser) {
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+                driver.set(new ChromeDriver());
+                break;
+        }
+
+        return driver.get();
+    }
+
+
+    public WebDriver createDriverGrid() {
+        String hubUrl = "http://10.25.59.112:4444/wd/hub";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        System.out.println("Starting " + browser + " on grid");
+
+        // Creating driver
+        switch (browser) {
+            case "chrome":
+                capabilities.setBrowserName(DesiredCapabilities.chrome().getBrowserName());
+                break;
+        }
+
+        try {
+            driver.set(new RemoteWebDriver(new URL(hubUrl), capabilities));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return driver.get();
+    }
+
+
+}
